618d927398d49393603e91b78e4bdf48175dd990
Jesse Sopel <jesse.sopel@gmail.com>
2024-11-02 16:20:37 -0400

    fix: Revert version to deploy beta

--- ember_mug/__init__.py
+++ ember_mug/__init__.py
@@ -1,8 +1,9 @@
 """Top-level package for Python Ember Mug."""
+
 from .mug import EmberMug
 
 __all__ = ("EmberMug",)
 
 __author__ = """Jesse Sopel"""
 __email__ = "jesse.sopel@gmail.com"
-__version__ = "1.1.0"
+__version__ = "1.0.1"
